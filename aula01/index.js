5dc5118326e4c7a25e87e24b50dc2f9d4741bf0d
Luana-Santos27 <luanadsantos27@yahoo.com.br>
2020-10-19 22:09:24 -0300

    adicionando aula de git

--- aula01/index.js
+++ aula01/index.js
@@ -1,1 +1,17 @@
 console.log("Hello World! Mãe tô na DH lecionando para uma galera massa")
+Na  aula  de  hoje  abordamos sobre: conflitos ,  criação  de  branch ,  como  trocar
+para  uma  branch  já  existente ,  criação  de  pull  request  e  como  adicionar  um  revisor
+para  revisar  Nosso  código  that  Estamos  mexendo  em  determinada  ramo .
+
+Resumo  de  comandos utilizados :
+  git  add  ( adicionar  sem  estágio )
+  git  commit  - m  "mensagem"  ( Preço total: Adicionar  Mensagem  para  Pacote  de  alterações )
+  git  puxar  ( Traz  alterações  fazer  Repositório  remoto  para  Nosso  Repositório  locais
+  ,  OU ,  busca  alinhar  como  alterações  fazer  Github  com  como  alterações  da  Nossa  máquina )
+  git  impulso  ( Envia  como  alterações  that  estao  nenhuma  fase  para  o  Github  efetivamente )
+
+  git  checkout  ( altera  da  filial  atual  para  uma  outra  filial  existente )
+  git  checkout  - b  nomeDaBranch  ( efetua  a  criação  de  uma  filial ,  e  acaba  "clonando"
+  um  version  ( TODOS  Arquivos  fazer  Repositório ) ,  com  base de  na  ramificar  that  estava  nenhum  momento
+  em  Que  Criou  uma  filial  nova ,  OU  SEJA ,  SE  rápido Você  CRIAR  Uma  filial  X  a  Partir  da  mestre
+  um  ramo  X  terá  todos  Arquivos  that  um  ramo  principal  tinha  Até  Aquele  momento )
